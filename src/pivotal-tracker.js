536a18e0204bfa395d73e7fa18865919c41ce10c
JP Quicksall <John.Quicksall1@T-Mobile.com>
2016-12-09 16:58:06 -0800

    Wrote the base create function

--- src/pivotal-tracker.js
+++ src/pivotal-tracker.js
@@ -24,14 +24,37 @@
   https = require('https');
 
 // Global Variables
+  var pivotal-tracker-url = "https://www.pivotaltracker.com/services/v5/";
+  var TRACKER_PROEJCT_ID = process.env.TRACKER_PROEJCT_ID;
+  var TRACKER_API_TOKEN = process.env.TRACKER_API_TOKEN;
 
 // hubot message functions
   module.exports = function(robot) {
+// Example scripts
     robot.respond(/hello/, function(msg){
       return msg.reply("hello!");
     });
     robot.hear(/orly/, function(msg){
       return msg.send("yarly");
+    });
+// Create a story
+    robot.respond(/create me[\sa]{1,3}story titled (.*\w*)/i, function(msg){
+      var name = msg.match[1];
+      var tracker_user_id = robot.brain.get('TrackerID'+msg.message.user.id)
+      data = {
+        "name":name,
+        "estimate":1,
+        "current_state":"unstarted",
+        "requested_by_id":tracker_user_id
+      }
+      return robot.http(pivotal-tracker-url + "projects/" + TRACKER_PROEJCT_ID + "/stories")
+        .post(data)(function(err, res, body) {
+          if (err){
+            robot.emit('error', err);
+          } else {
+            return msg.send("" + body):
+          }
+        });
     });
   }
 }).call(this);
